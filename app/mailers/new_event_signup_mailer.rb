92c2eabc0921e01132eba75b23e74dcaaf1d6a93
Student <example@example.com>
2020-02-06 09:47:13 -0800

    add card for event sign up

--- app/mailers/new_event_signup_mailer.rb
+++ app/mailers/new_event_signup_mailer.rb
@@ -2,7 +2,7 @@ class NewEventSignupMailer < ApplicationMailer
   def notify_signup(user, event)
     @event = event
     @user = user
-    binding.pry
+    # binding.pry
     mail(to: @user.user_email, subject: "Fun Awaits...")
   end
 end
